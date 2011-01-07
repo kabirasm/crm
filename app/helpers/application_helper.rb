fc5b56280e4e27f14e021a469b4e40f35d98d317
Kabir Mohammed <kabirasm@gmail.com>
2011-01-07 02:09:21 +0000

    Home page is live

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
+# Essential based on the railstutorial.org - Listing 4.2 Defining a title helper accessed 06/Jan/2011 22:56hrs
+
+
 module ApplicationHelper
+
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Customer Relationship Manager"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
+
